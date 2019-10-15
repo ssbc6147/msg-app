45929c29ae85ba95ec39ec4eefaa3fc207c2bbf0
ssbc6147 <6147mgcn@gmail.com>
2019-10-15 16:47:44 +0900

    msg-app改良

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,8 @@ class MessagesController < ApplicationController
     def show
         @msg = params[:msg]
     end
+    def show2
+        @msg1 = params[:msg1]
+        @msg2 = params[:msg2]
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'messages/:msg' => 'messages#show'
+  get 'messages/:msg1/:msg2' => 'messages#show2'
 end
 
